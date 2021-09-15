a93c76a95c2f6c9cf71e9f835d45577aa946d2f9
fuboki10 <abdelrahman.tarek1910@gmail.com>
2021-09-15 15:50:44 +0200

    add api key

--- website/app.js
+++ website/app.js
@@ -1,5 +1,10 @@
 /* Global Variables */
+const openWeatherApiKey = "8459af060455c7b36006073e9bb5d83a";
+const openWeatherUrl = `http://maps.openweathermap.org/maps/2.0/weather?appid=${openWeatherApiKey}`;
+const backendUrl = "http://127.0.0.1:3000/"
 
 // Create a new date instance dynamically with JS
-let d = new Date();
-let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
+const getDate = () => {
+  const d = new Date();
+  return d.getMonth() + '.' + d.getDate() + '.' + d.getFullYear();
+}
